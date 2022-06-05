d1145f958f229a69bf4c0c163a96e1fcf6f97709
GowravKrishna-V <gowravgdboy@gmail.com>
2022-06-05 17:50:57 +0530

    third(2) commit

--- lib/components/invoice_card.dart
+++ lib/components/invoice_card.dart
@@ -1,90 +1,105 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:invoice_generator/model/invoice.dart';
 
 class InvoiceCard extends StatelessWidget {
-  const InvoiceCard({Key? key}) : super(key: key);
-
+  const InvoiceCard(
+      {Key? key,
+      required this.invoiceNumber,
+      required this.customerName,
+      required this.amount,
+      required this.status})
+      : super(key: key);
+  final String invoiceNumber;
+  final String customerName;
+  final double amount;
+  final String status;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: const Color(0xffFFB400),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(
-              0.2,
-              3.0,
-            ),
-            blurRadius: 7.0,
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                "Invoice Number",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Color(0xff2D4059),
-                  fontWeight: FontWeight.bold,
-                ),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xffFFB400),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(
+                0.2,
+                3.0,
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100),
-                  color: Color(0xffF6F6F6),
+              blurRadius: 7.0,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  invoiceNumber,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xff2D4059),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                padding: EdgeInsets.all(3),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.circle,
-                      size: 15,
-                      color: Color(0xffEA5455),
-                    ),
-                    Text(
-                      "Overdue",
-                      style: TextStyle(
-                        fontSize: 15,
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Color(0xffF6F6F6),
+                  ),
+                  padding: EdgeInsets.all(3),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.circle,
+                        size: 15,
                         color: Color(0xffEA5455),
-                        fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ],
+                      Text(
+                        status,
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color(0xffEA5455),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              customerName,
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xffF6F6F6),
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Customer Name",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color(0xffF6F6F6),
-              fontWeight: FontWeight.bold,
             ),
-          ),
-          SizedBox(
-            height: 4,
-          ),
-          const Text(
-            "Amount",
-            style: TextStyle(
-              fontSize: 20,
-              color: Color(0xffF6F6F6),
-              fontWeight: FontWeight.bold,
+            SizedBox(
+              height: 4,
             ),
-          ),
-        ],
+            Text(
+              amount.toString(),
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xffF6F6F6),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
